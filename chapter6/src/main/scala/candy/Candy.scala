b1e49219549628b4f6ff7953c49275bdc585c82e
bigyo <kdy2353@gmail.com>
2021-05-07 13:53:11 +0900

    candy

--- chapter6/src/main/scala/candy/Candy.scala
+++ chapter6/src/main/scala/candy/Candy.scala
@@ -1,3 +1,9 @@
+package candy
+
+import scala.collection.immutable
+import state.State
+import state.State._
+
 sealed trait Input
 case object Coin extends Input
 case object Turn extends Input
@@ -5,11 +11,23 @@ case object Turn extends Input
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Candy {
-  def update = ???
-    // (i: Input) => (s: Machine) =>
-    // (i, s) match {
-    // }
+  def update = (i: Input) => (s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) => Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) => Machine(true, candy - 1, coin)
+  }
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
-  
+  // inputs 를 State로 바꾼다.
+  // map foldRight unit modify get 
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    s <- get
+    _ <- inputs.map( a -> )
+  } yield ()
+
+  def main(args: Array[String]): Unit = {
+    val list = List(Machine(true, 2, 1))
+    // println(simulateMachine())
+  }
 }
--- chapter6/src/main/scala/rng/Rng.scala
+++ chapter6/src/main/scala/rng/Rng.scala
@@ -1,30 +1,23 @@
+package rng
+
 trait RNG {
-  def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
+  def nextInt: (Int, RNG)
 }
 
 object RNG {
-  // NB - this was called SimpleRNG in the book text
 
   case class Simple(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
-      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
-      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
-      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL 
+      val nextRNG = Simple(newSeed) 
+      val n = (newSeed >>> 16).toInt 
+      (n, nextRNG)
     }
   }
-
-  // We need to be quite careful not to skew the generator.
-  // Since `Int.Minvalue` is 1 smaller than `-(Int.MaxValue)`,
-  // it suffices to increment the negative numbers by 1 and make them positive.
-  // This maps Int.MinValue to Int.MaxValue and -1 to 0.
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
     (if (i < 0) -(i + 1) else i, r)
   }
-
-  // We generate an integer >= 0 and divide it by one higher than the
-  // maximum. This is just one possible solution.
   def double(rng: RNG): (Double, RNG) = {
     val (i, r) = nonNegativeInt(rng)
     (i / (Int.MaxValue.toDouble + 1), r)
@@ -51,11 +44,6 @@ object RNG {
     ((d1, d2, d3), r3)
   }
 
-  // There is something terribly repetitive about passing the RNG along
-  // every time. What could we do to eliminate some of this duplication
-  // of effort?
-
-  // A simple recursive solution
   def ints(count: Int)(rng: RNG): (List[Int], RNG) =
     if (count == 0)
       (List(), rng)
@@ -65,7 +53,6 @@ object RNG {
       (x :: xs, r2)
     }
 
-  // A tail-recursive solution
   def ints2(count: Int)(rng: RNG): (List[Int], RNG) = {
     def go(count: Int, r: RNG, xs: List[Int]): (List[Int], RNG) =
       if (count == 0)
@@ -93,14 +80,6 @@ object RNG {
   val _double: Rand[Double] =
     map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 
-  // This implementation of map2 passes the initial RNG to the first argument
-  // and the resulting RNG to the second argument. It's not necessarily wrong
-  // to do this the other way around, since the results are random anyway.
-  // We could even pass the initial RNG to both `f` and `g`, but that might
-  // have unexpected results. E.g. if both arguments are `RNG.int` then we would
-  // always get two of the same `Int` in the result. When implementing functions
-  // like this, it's important to consider how we would test them for
-  // correctness.
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rng => {
       val (a, r1) = ra(rng)
@@ -117,21 +96,9 @@ object RNG {
   val randDoubleInt: Rand[(Double, Int)] =
     both(double, int)
 
-  // In `sequence`, the base case of the fold is a `unit` action that returns
-  // the empty list. At each step in the fold, we accumulate in `acc`
-  // and `f` is the current element in the list.
-  // `map2(f, acc)(_ :: _)` results in a value of type `Rand[List[A]]`
-  // We map over that to prepend (cons) the element onto the accumulated list.
-  //
-  // We are using `foldRight`. If we used `foldLeft` then the values in the
-  // resulting list would appear in reverse order. It would be arguably better
-  // to use `foldLeft` followed by `reverse`. What do you think?
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
 
-  // It's interesting that we never actually need to talk about the `RNG` value
-  // in `sequence`. This is a strong hint that we could make this function
-  // polymorphic in that type.
 
   def _ints(count: Int): Rand[List[Int]] =
     sequence(List.fill(count)(int))
@@ -139,7 +106,7 @@ object RNG {
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
     rng => {
       val (a, r1) = f(rng)
-      g(a)(r1) // We pass the new state along
+      g(a)(r1) 
     }
 
   def nonNegativeLessThan(n: Int): Rand[Int] = {
